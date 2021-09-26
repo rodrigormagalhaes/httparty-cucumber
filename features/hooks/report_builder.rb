9429d46642e38c11ec1d0bdb874e3c20a639ebe7
Rodrigo Magalhães <rodrigorm.rm@gmail.com>
2021-09-26 16:21:18 -0300

    Ajustes para report cucumber jenkins

---
+++ features/hooks/report_builder.rb
@@ -0,0 +1,15 @@
+require 'report_builder'
+require 'date'
+
+at_exit do
+  ReportBuilder.configure do |config|
+    config.input_path = 'reports/report.json'
+    config.report_path = 'reports/report'
+    config.report_types = [:html]
+    config.report_title = "Report Test"
+    config.voice_commands = true
+    config.color = 'green'
+    config.additional_info = {DateTime: DateTime.now, Project: 'teste_'}
+  end
+  ReportBuilder.build_report
+end
